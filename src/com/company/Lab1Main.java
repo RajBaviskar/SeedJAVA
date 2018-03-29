76ff32da715884ecfca9b2999ecba9363c894dd2
Rajendra <rajendra.baviskar@cerner.com>
2018-03-29 16:18:13 -0500

    adding lab 1

---
+++ src/com/company/Lab1Main.java
@@ -0,0 +1,9 @@
+package com.company; 
+import com.company.MyDate;
+
+public class Lab1Main {
+public static void main(String[] args) {
+		MyDate birthDay = new MyDate(20,07,1987);
+		birthDay.displayDate();
+	}
+}
---
+++ src/com/company/MyDate.java
@@ -0,0 +1,48 @@
+package com.company; 
+
+public class MyDate {
+	private long day;
+	private long month;
+	private long year;
+
+
+	public MyDate() {
+		this.day = 0;
+		this.month = 0;
+		this.year = 0;
+	}
+
+    public MyDate(final long day, final long month, final long year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+
+	public void displayDate() {
+		System.out.println(day + "/" + month + "/" + year);
+	}
+
+	public void setDay(final long day) {
+		this.day = day;
+	}
+
+	public void setMonth(final long day) {
+		this.day = day;
+	}
+
+	public void setYear(final long day) {
+		this.day = day;
+	}
+
+	public long getDay() {
+		return this.day;
+	}
+
+	public long getMonth() {
+		return this.month;
+	}
+
+	public long getYear() {
+		return this.year;
+	}
+}
